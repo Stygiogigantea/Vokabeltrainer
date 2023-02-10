a6c68f66ba4dee62d85662f578be06a01bd831c3
FJ31415 <109368000+FJ31415@users.noreply.github.com>
2023-02-10 12:43:11 +0100

    Add files via upload

---
+++ src/datenspeicherung/Speicher.java
@@ -0,0 +1,12 @@
+package speicher;
+
+import fachkonzept.Vokabel;
+
+public class Speicher {
+    public Vokabel[] liesVokabeln(String dateiname){
+        //TODO
+    }
+    public schreibeVokabeln(Stirng dateiname, Vokabel[] vokabeln){
+        //TODO
+    }
+}
