24aecde044e375860a382fb1bce5396c4ff551b8
FJ31415 <109368000+FJ31415@users.noreply.github.com>
2023-02-10 12:38:48 +0100

    Add files via upload

--- src/fachkonzept/Kasten.java
+++ src/fachkonzept/Kasten.java
@@ -1,4 +1,36 @@
 package fachkonzept;
 public class Kasten {
-    
+    public void aendereVokabel(int vokabelNr, String aufgabe, String ergebnis){
+        //TODO
+    }
+    private boolean alleVokabelnGekonnt(){
+
+    }
+    public void fuegeVokabelHinzu(String aufgabe, String ergebnis){
+        //TODO
+    }
+    public Vokabel gibNaechsteVokabel(){
+        
+    }
+    private int gibNaechsteVokabelNr(){
+
+    }
+    public void ladeVokabeln(String dateiname){
+        //TODO
+    }
+    public Vokabel[] liesVokabeln(){
+
+    }
+    public void loescheVokabeln(int vokabelNr){
+        //TODO
+    }
+    public void nimmVokabelZurueck(Vokabel vokabel){
+        //TODO
+    }
+    private void setzeZurueckGekonnt(){
+
+    }
+    public void speichereVokabeln(String dateiname){
+        //TODO
+    }
 }
