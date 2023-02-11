b72ed69d7da415ed0448a80a5d559cb3daad3e98
FJ31415 <109368000+FJ31415@users.noreply.github.com>
2023-02-11 01:44:03 +0100

    Add files via upload
    Added uncoded methodes to: Speicher

--- src/datenspeicherung/Speicher.java
+++ src/datenspeicherung/Speicher.java
@@ -1,12 +1,13 @@
-package datenspeicherung;
+package speicher;
 
+import java.util.ArrayList;
 import fachkonzept.Vokabel;
 
 public class Speicher {
-    public Vokabel[] liesVokabeln(String dateiname){
+    public ArrayList<Vokabel> liesVokabeln(String dateiname){
         //TODO
     }
-    public schreibeVokabeln(Stirng dateiname, Vokabel[] vokabeln){
+    public schreibeVokabeln(Stirng dateiname, java.util.ArrayList<Vokabel> vokabeln){
         //TODO
     }
 }
