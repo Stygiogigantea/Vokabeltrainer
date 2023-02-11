546ec2754baee2f7eea8b0d50f39aa06a421d9f7
FJ31415 <109368000+FJ31415@users.noreply.github.com>
2023-02-11 01:42:41 +0100

    Add files via upload
    added methods to: Kasten

--- src/fachkonzept/Kasten.java
+++ src/fachkonzept/Kasten.java
@@ -1,36 +1,66 @@
 package fachkonzept;
+
+import datenspeicherung.Speicher;
+import java.util.ArrayList;
+
 public class Kasten {
+    private ArrayList<Vokabel> vokabeln;
+    private Speicher derSpeicher;
+    
+    public Kasten(){
+        derSpeicher = new Speicher();
+    }
+
     public void aendereVokabel(int vokabelNr, String aufgabe, String ergebnis){
-        //TODO
+        for(int i = 0; i < vokabeln.size(); i++)
+            if(vokabeln.get(i).liesVokabelNr()==vokabelNr)
+                vokabeln.set(i, new Vokabel(vokabelNr, aufgabe, ergebnis,false));
     }
+
     private boolean alleVokabelnGekonnt(){
-
+     for(int i = 0; i < vokabeln.size(); i++){
+        if(!vokabeln.get(i).istGekonnt())
+            return false;
+     }   
+     return true;
     }
+
     public void fuegeVokabelHinzu(String aufgabe, String ergebnis){
-        //TODO
+        vokabeln.add(new Vokabel(gibNaechsteVokabelNr(), aufgabe, ergebnis, false));
     }
+
     public Vokabel gibNaechsteVokabel(){
-        
+        return vokabeln.get(gibNaechsteVokabelNr());
     }
+
     private int gibNaechsteVokabelNr(){
-
+        return vokabeln.size();
     }
+
     public void ladeVokabeln(String dateiname){
-        //TODO
+        vokabeln = derSpeicher.liesVokabeln(dateiname);
     }
-    public Vokabel[] liesVokabeln(){
 
+    public ArrayList<Vokabel> liesVokabeln(){
+        return vokabeln;
     }
-    public void loescheVokabeln(int vokabelNr){
-        //TODO
+
+    public void loescheVokabel(int vokabelNr){
+        for(int i = 0; i < vokabeln.size();i++)
+            if(vokabeln.get(i).liesVokabelNr()==vokabelNr)
+                vokabeln.remove(vokabelNr);
     }
+
     public void nimmVokabelZurueck(Vokabel vokabel){
         //TODO
     }
-    private void setzeZurueckGekonnt(){
 
+    private void setzeZurueckGekonnt(){
+        for(int i = 0; i< vokabeln.size(); i++)
+            vokabeln.get(i).setzeGekonnt(true);
     }
+
     public void speichereVokabeln(String dateiname){
-        //TODO
+        derSpeicher.schreibeVokabeln(dateiname, vokabeln);
     }
 }
