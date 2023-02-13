bcc2df03746e98a9a9856a0b3477064a492b9e4e
FJ31415 <109368000+FJ31415@users.noreply.github.com>
2023-02-13 07:08:01 +0100

    Add files via upload
    added methods to: SteuerungVerwalten, SteuerungLernen

--- src/steuerung/SteurungLernen.java
+++ src/steuerung/SteurungLernen.java
@@ -1,4 +1,21 @@
 package steuerung;
+
+import fachkonzept.Kasten;
+
 public class SteurungLernen {
+    private Kasten derKasten;
     
+    public SteurungLernen(){
+        derKasten = new Kasten();
+    }
+
+    public void geklicktGebenAufgabe(){
+        derKasten.gibNaechsteVokabel();
+    }
+    public void geklicktLaden(String dateiname){
+        derKasten.ladeVokabeln(dateiname);
+    }
+    public void geklicktPruefenVermutung(String vermutung){
+        //TODO
+    }
 }
