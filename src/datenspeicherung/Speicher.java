04aacebd004224300a744a00390cd3daaa7d86db
Stygiogigantea <119011264+Stygiogigantea@users.noreply.github.com>
2023-03-01 10:22:31 +0100

    Stirng to String

--- src/datenspeicherung/Speicher.java
+++ src/datenspeicherung/Speicher.java
@@ -7,7 +7,7 @@ public class Speicher {
     public ArrayList<Vokabel> liesVokabeln(String dateiname){
         //TODO
     }
-    public schreibeVokabeln(Stirng dateiname, ArrayList<Vokabel> vokabeln){
+    public schreibeVokabeln(String dateiname, ArrayList<Vokabel> vokabeln){
         //TODO
     }
 }
