286fc60493b7c5a79a8ffe13d97756beb5d055a3
FJ31415 <109368000+FJ31415@users.noreply.github.com>
2023-05-03 09:33:23 +0200

    Update datenspeicherung 03.05.2023

--- src/datenspeicherung/Speicher.java
+++ src/datenspeicherung/Speicher.java
@@ -1,5 +1,6 @@
 package datenspeicherung;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,8 +15,15 @@ public class Speicher
 	private Connection con;
 	private PreparedStatement stmt;
 	private ResultSet rs;
-	private String accdbLocation  = "jdbc:ucanaccess://C://Users//Janhm//OneDrive//Dokumente//12 Klasse//IT SW//Eclipse_DBs//VokabeltrainerDB.accdb";
-
+	private String accdbLocation; 
+	
+	public Speicher() {
+		System.out.println(new File("VokabeltrainerDB.accdb").getAbsolutePath());
+		accdbLocation = "jdbc:ucanaccess:VokabeltrainerDB.accdb";
+		//TODO update
+		
+	}
+	
 	public ArrayList<Vokabel> liesVokabeln(String dateiname) throws Exception
 	{
 		oeffneDatenbank();
@@ -44,8 +52,8 @@ public class Speicher
 		return ergebnis;
 
 	}
-	
-	private int getKastenNr(String dateiname) throws Exception
+
+	private int getKastenNr(String dateiname) throws Exception //TODO remove
 	{
 		oeffneDatenbank();
 
@@ -72,37 +80,64 @@ public class Speicher
 		return erg;
 	}
 
-	public void schreibeVokabeln(String dateiname, ArrayList<Vokabel> vokabeln) throws Exception
+	public void schreibeVokabeln(String dateiname, ArrayList<Vokabel> vokabeln)
+			throws Exception
 	{
 		oeffneDatenbank();
-		String sqlStmt = "INSERT INTO vokabel (term, verkaufspreis, lagerbestand) ";
-		//TODO
+
+		for (int i = 0, l = vokabeln.size(); i < l; i++)
+		{
+			String sqlStmt = "INSERT INTO vokabel (id, term, ergebnis, gekonnt)";
+			sqlStmt += " VALUES (?, ?, ?, ?)";
+
+			try
+			{
+				stmt = con.prepareStatement(sqlStmt);
+
+				// setze "?" werte
+				stmt.setInt(1, vokabeln.get(i).liesVokabelNr());
+				stmt.setString(2, vokabeln.get(i).liesAufgabe());
+				stmt.setString(3, vokabeln.get(i).liesErgebnis());
+				stmt.setBoolean(4, vokabeln.get(i).istGekonnt());
+
+				// querry ausführen
+				stmt.executeUpdate();
+			}
+			catch (SQLException e)
+			{
+				throw new Exception("Fehler beim Hinzufügen des Produkts!");
+			}
+		}
+
+		schliesseDatenbank();
 	}
 
 	private void oeffneDatenbank() throws Exception
-    {
-    	try {
-    		con=DriverManager.getConnection(accdbLocation);
-    	}
-    	catch (SQLException e)
-    	{
-    		throw new Exception("Fehler beim oeffnen der Datenbank!");
-    	}
-    }
+	{
+		try
+		{
+			con = DriverManager.getConnection(accdbLocation);
+		}
+		catch (SQLException e)
+		{
+			throw new Exception("Fehler beim oeffnen der Datenbank!");
+		}
+	}
 
 	private void schliesseDatenbank() throws Exception
-    {
-    	try {
-    		if(rs!=null)
-    		{
-    			rs.close();
-    		}
-    		stmt.close();
-    		con.close();
-    	}
-    	catch(SQLException e)
-    	{
-    		throw new Exception("Fehler beim Schliessen der Datenbank!");
-    	}
-    }
+	{
+		try
+		{
+			if (rs != null)
+			{
+				rs.close();
+			}
+			stmt.close();
+			con.close();
+		}
+		catch (SQLException e)
+		{
+			throw new Exception("Fehler beim Schliessen der Datenbank!");
+		}
+	}
 }
