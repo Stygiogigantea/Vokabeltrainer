04e9d6d2230f3e4a757cce0887fe8358d6855709
Stygiogigantea <119011264+Stygiogigantea@users.noreply.github.com>
2023-06-21 10:39:29 +0200

    fix bla bla bla 			#

--- src/steuerung/SteuerungLernen.java
+++ src/steuerung/SteuerungLernen.java
@@ -9,19 +9,11 @@ public class SteuerungLernen {
         derKasten = new Kasten();
     }
 
-<<<<<<< HEAD
-    public void geklicktGebenAufgabe() throws Exception{
-        derKasten.gibNaechsteVokabel();
-    }
-    public void geklicktLaden(String dateiname) throws Exception{
-        derKasten.ladeVokabeln(dateiname);
-=======
     public void geklicktGebenAufgabe(){
         derKasten.gibNaechsteVokabel();
     }
     public void geklicktLaden(String dateiname) throws Exception{
         derKasten.ladeVokabeln(dateiname); //evtl Vokabel als Text returnen um sie in der GUI zu laden
->>>>>>> 4f125b29ef88922f6b7e71c121c17b69637a1887
     }
     public void geklicktPruefenVermutung(String vermutung){
         //TODO
