7263521a7fd2e15be038bd9fdfee4d58881467fc
Stygiogigantea <119011264+Stygiogigantea@users.noreply.github.com>
2023-06-21 10:29:35 +0200

    LerneGUI

--- src/benutzerschnittstelle/LernenBenutzerschnittstelle.java
+++ src/benutzerschnittstelle/LernenBenutzerschnittstelle.java
@@ -118,3 +118,8 @@ public class LernenBenutzerschnittstelle extends JFrame
 		contentPane.add(textDefinitionEingabe);
 		textDefinitionEingabe.setColumns(10);
 	}
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> 4f125b29ef88922f6b7e71c121c17b69637a1887
